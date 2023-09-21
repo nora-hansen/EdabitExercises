93944e3d6ad04f310ac18455967e7c5c4b673280
Nora H <nh080799@gmail.com>
2023-09-21 20:46:17 +0200

    Challenge in C++

---
+++ C++/Challenge03_Burrrrrp/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+std::string burp(int n);
+
+int main()  {
+    std::cout << burn(9000);
+    
+    return 0;
+}
+
+std::string burp(int n) {
+    std::string result = "Bu";
+    for (int i = 0; i < n; i++) {
+        result += "r";
+    }
+    result += "p";
+    return result;
+}
