0dbac222e4a53f420854d39145c6e87c6630df86
Nora H <nh080799@gmail.com>
2023-09-21 21:19:37 +0200

    Added Java challenges!!

---
+++ Java/Challenge01_SumOfTwoInts/sumOfTwoNumbers/sumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package Java.Challenge01_SumOfTwoInts.sumOfTwoNumbers;
+
+/**
+ * Help how do I do Java
+ */
+public class sumOfTwoNumbers    {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
