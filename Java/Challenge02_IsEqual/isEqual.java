37a3c11db30e7b13c72dc7e6eb49c5e518bac0c8
Nora H <nh080799@gmail.com>
2023-09-22 21:06:34 +0200

    Some challenges

---
+++ Java/Challenge02_IsEqual/isEqual.java
@@ -0,0 +1,7 @@
+package Java.Challenge02_IsEqual;
+
+public class isEqual    {
+    public static boolean isEqualFunc(int a, int b) {
+        return a == b;
+    }
+}
---
+++ Java/Challenge03_MinToSec/minToSec.java
@@ -0,0 +1,7 @@
+package Java.Challenge03_MinToSec;
+
+public class minToSec {
+    public static int convert(int minutes)  {
+        return minutes*60;
+    }
+}
