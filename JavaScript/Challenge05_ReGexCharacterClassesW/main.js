ca5fbfdb63351fddb1cb1ac9394deeb4275fde81
Nora H <nh080799@gmail.com>
2023-10-11 13:38:33 +0200

    JavaScript Regexp challenge

---
+++ JavaScript/Challenge05_ReGexCharacterClassesW/main.js
@@ -0,0 +1,6 @@
+// Challenge 05
+//  Find out how many <div> elements are used in a string. 
+//  Use the character class \W in your expression.
+//
+//  Hmm, how to make it ignore </div>? :s
+const REGEXP = /<div\W/g
